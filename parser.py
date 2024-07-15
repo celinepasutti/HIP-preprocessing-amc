6d7b55eec43088a6ade267dc624c2ecae98f97f9
hi337 <ajamiehrateb@gmail.com>
2024-07-15 14:29:54 -0600

    processing a single amc file

--- parser.py
+++ parser.py
@@ -4,9 +4,14 @@ parsed_amc = ParseAMC("C://coding//mocapPlayer//79_01.amc")
 
 arr = []
 
-for feature in parsed_amc.amc[0]:
-    for val in parsed_amc.amc[0][feature]:
-        arr.append(val)
+for frame in parsed_amc.amc:
+    frame_arr = []
+    for feature in frame:
+        for f in frame[feature]:
+            frame_arr.append(f)
 
-print(len(arr))
+    arr.append(frame_arr)
+
 print(arr)
+print(len(arr))
+print(len(arr[0]))
