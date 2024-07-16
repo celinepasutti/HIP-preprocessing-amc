b754f9cd2b76b82a86ef3d7e4769d16b84a6b941
hi337 <ajamiehrateb@gmail.com>
2024-07-16 11:59:12 -0600

    new parser

---
+++ gpt_parser.py
@@ -0,0 +1,35 @@
+from asfamcparser import ParseAMC
+
+parsed_amc = ParseAMC("C://coding//mocapPlayer//79_01.amc")
+
+def batching(amc, batch_size):
+    flattened_frames = []
+
+    # Flatten the frames
+    for frame in amc:
+        frame_arr = []
+        for feature in frame:
+            for f in frame[feature]:
+                frame_arr.append(f)
+        flattened_frames.append(frame_arr)
+
+    batched_arr = []
+    total_frames = len(flattened_frames)
+    num_batches = (total_frames + batch_size - 1) // batch_size  # Calculate number of batches
+
+    # Create batches with padding for the last batch if necessary
+    for i in range(num_batches):
+        start_idx = i * batch_size
+        end_idx = start_idx + batch_size
+        batch = flattened_frames[start_idx:end_idx]
+        
+        # Check if padding is needed for the last batch
+        if len(batch) < batch_size:
+            padding_needed = batch_size - len(batch)
+            batch.extend(flattened_frames[:padding_needed])
+        
+        batched_arr.append(batch)
+    
+    return batched_arr
+
+print(len(batching(parsed_amc.amc, 32)[19]))
