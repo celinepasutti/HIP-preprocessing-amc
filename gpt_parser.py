7f5e11e8c8136370d63eed37408bea035f06dcd4
hi337 <ajamiehrateb@gmail.com>
2024-07-16 14:30:27 -0600

    without saving

--- gpt_parser.py
+++ gpt_parser.py
@@ -5,7 +5,7 @@ import os
 
 folder_path = "C://coding//HIP-preprocessing-amc//mocapPlayer"
 
-batch_size = 128
+batch_size = 32
 
 master_list = []
 
@@ -20,7 +20,7 @@ def batching(amc, batch_size):
                 frame_arr.append(f)
         flattened_frames.append(frame_arr)
 
-    batched_arr = []
+    # batched_arr = []
     total_frames = len(flattened_frames)
     num_batches = math.ceil(total_frames / batch_size)  # Calculate number of batches
 
@@ -37,9 +37,9 @@ def batching(amc, batch_size):
             batch.extend(flattened_frames[:padding_needed])
         
         
-        batched_arr.append(batch)
-    
-    master_list.extend(batched_arr)
+        # batched_arr.append(batch)
+        master_list.append(batch)
+    # master_list.extend(batched_arr)
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".amc"):
@@ -48,6 +48,6 @@ for filename in os.listdir(folder_path):
         batching(parsed_amc, batch_size)
 
 
-a_tensor =  torch.tensor(master_list)
+a_tensor =  torch.tensor(master_list, dtype=torch.float32)
 
 print(a_tensor.shape)
